3398a4bcbf5d1b0ae25e87c72d4c24fd86c06cc0
Sergio Nizama <s.nizama@gmail.com>
2024-03-18 02:51:26 +0900

    New: - oklch2rgb.ts utility functions for color conversion.
    Update:
    - dynamically updating cube color on theme switch.

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,6 +1,14 @@
 import { useFrame, type MeshProps } from '@react-three/fiber'
-import { useRef, useState } from 'react'
-import { type Mesh } from 'three'
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { type Mesh, Color } from 'three'
+import { oklch2rgb } from '../util/oklch2rgb'
+
+function getThemeColors() {
+  const compStyle = getComputedStyle(document.documentElement)
+  const primary = compStyle.getPropertyValue('--p')
+  const secondary = compStyle.getPropertyValue('--s')
+  return [primary, secondary]
+}
 
 export function Box(props: MeshProps) {
   // This reference will give us direct access to the mesh
@@ -8,9 +16,38 @@ export function Box(props: MeshProps) {
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
+
+  const [materialColors, setMaterialColors] = useState<string[]>(
+    getThemeColors()
+  )
+
+  const materialColor = useMemo(() => {
+    if (materialColors.length === 0) return new Color()
+    const color = hovered ? materialColors[1] : materialColors[0]
+    const lch = color.split(' ').map(val => parseFloat(val))
+    const [r, g, b] = oklch2rgb(lch)
+    return new Color(r, g, b)
+  }, [hovered, materialColors])
+
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((_, delta) => (meshRef.current.rotation.x += delta))
-  // Return view, these are regular three.js elements expressed in JSX
+
+  useEffect(() => {
+    const observer = new MutationObserver(muts => {
+      if (muts[0].attributeName === 'data-theme') {
+        setMaterialColors(getThemeColors())
+      }
+    })
+    observer.observe(document.documentElement, {
+      attributes: true,
+      childList: false,
+      characterData: false,
+    })
+    return () => {
+      observer.disconnect()
+    }
+  }, [])
+
   return (
     <mesh
       {...props}
@@ -21,7 +58,7 @@ export function Box(props: MeshProps) {
       onPointerOut={event => setHover(false)}
     >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'turquoise'} />
+      <meshStandardMaterial color={materialColor} />
     </mesh>
   )
 }
