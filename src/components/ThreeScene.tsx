f2e4ea19731dff5b678e588e4b15263ea799cc59
Sergio Nizama <s.nizama@gmail.com>
2024-03-18 03:23:20 +0900

    added synthwave theme and showing border around canvas

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -1,9 +1,10 @@
 import { Canvas } from '@react-three/fiber'
 import { Box } from './Box'
+import { OrbitControls } from '@react-three/drei'
 
 export function ThreeScene() {
   return (
-    <Canvas>
+    <Canvas className='border-primary border rounded-md'>
       <ambientLight intensity={Math.PI / 2} />
       <spotLight
         position={[10, 10, 10]}
@@ -13,6 +14,7 @@ export function ThreeScene() {
         intensity={Math.PI}
       />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <OrbitControls />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
     </Canvas>
